2ae614c0af0061ee37259be740f1161d0a3fec71
harikrishnaperumolla <perumolla.harikrishna017@gmail.com>
2018-08-01 11:11:50 +0530

    m

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function loadJSON(file){
   return new Promise((resolve,request)=>{
     return fetch(file).then(response=>{
       if(response.ok){
-        resolve(response.json());
+        resolve(response.json())
       }
       else {
         reject(new Error("error"));
@@ -29,6 +29,7 @@ var fetchedData=loadJSON("data.json");
 fetchedData.then(data=>{
   console.log(data);
   carrer(data.carrer);
+  education(data.education);
 })
 var child2=document.querySelector("#child2");
 function carrer(car)
@@ -41,4 +42,19 @@ function carrer(car)
   var p=document.createElement("p");
   p.textContent=car.info;
   child2.appendChild(p);
+}
+
+function education(edu)
+{
+  var heading=document.createElement("h2");
+  heading.textContent="Education Qualification";
+  child2.appendChild(heading);
+  var hr=document.createElement("hr");
+  child2.appendChild(hr);
+  var table=document.createElement("table");
+  child2.appendChild(table);
+  var tr="<tr> <td> S.no </td> <td> DEGREE </td> <td> Institute </td> <td> data </td> </tr>";
+  table.innerHTML=tr;
+  table.border="1";
+
 }
